d4293794653bc89b2f152a56e6ac43bf5229ce2c
Juan Fernandez <jafern14@gmail.com>
2016-01-15 15:15:30 -0500

    test commit

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
-
-if git commit 
+STATUS=(git status)
+echo $STATUS
+if $STATUS | grep -q "Changes not staged for commit";
+    then
+        echo "The are changes that are not staged for commit. Please resolve first."
+        echo $STATUS
+elif $STATUS | grep -q "Changes to be committed";
     then
         echo "Please enter a git commit message: "
         read message
         git commit -m $message
-    else
-        echo "Wrong."
+else
+    echo "Wrong."
 fi;
