77ac60128c85ec55be0bb1ccf2753bd3ae4abbb7
Juan Fernandez <jafern14@gmail.com>
2016-01-15 14:41:30 -0500

    script dev

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
-!/bin/bash
 
-echo "Hello, World!"
+#!/usr/bin/env bash
+set -o errexit #abort if any command fails
+clear
+
+if  $(git commit | grep "nothing to commit")
+    then 
+        echo "Found" 
+    else 
+        echo "Not found" 
+fi;
