80bb7f9b42a3ad41af61770a18249ec9eefb939d
Juan Fernandez <jafern14@gmail.com>
2016-01-18 13:20:00 -0500

    Testing script commit

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
-STATUS=(git status)
-echo $STATUS
-if $STATUS | grep -q "Changes not staged for commit";
+STATUS='git status'
+if $STATUS | grep -q "Changes not staged for commit"
     then
-        echo "The are changes that are not staged for commit. Please resolve first."
-        echo $STATUS
-elif $STATUS | grep -q "Changes to be committed";
+        echo "The are changes that are not staged for commit. Are you sure you would like to ignore the changes and continue commiting? (y/n) "
+        read ANSWER
+        if $ANSWER -eq "y"
+            then
+                echo "Please enter a commit message: "
+                read MESSAGE
+                git commit -m $MESSAGE
+            else
+                clear
+                echo "Please review the following status and fix the pending items to be committed. "
+                echo
+                echo 'git status'
+        fi;
+elif $STATUS | grep -q "Changes to be committed"
     then
         echo "Please enter a git commit message: "
         read message
