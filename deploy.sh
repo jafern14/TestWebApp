014ccc00c11ca895e14696cdd9bdfc9752b2c9af
Juan Fernandez <jafern14@gmail.com>
2016-01-18 14:11:04 -0500

    This is going to work. I swear.

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,49 @@
-#!/usr/bin/env bash
-set -o errexit #abort if any command fails
-STATUS='git status'
-if $STATUS | grep -q "Changes not staged for commit"
-    then
-        echo "The are changes that are not staged for commit. Are you sure you would like to ignore the changes and continue commiting? (y/n) "
-        read ANSWER
-        if [ "$ANSWER" == "y" ]
-            then
-                clear
-                echo "Please enter a commit message: "
-                read -e MESSAGE
-                git commit -m "$MESSAGE"
-                git push
-            else
-                clear
-                echo "Please review the following status and fix the pending items to be committed. "
-                echo
-                git status
-        fi;
-elif $STATUS | grep -q "Changes to be committed"
-    then
-        echo "Please enter a git commit message: " 
-        read MESSAGE
-        git commit -m "$MESSAGE"
-        git push
-else 
-    git push
-fi;
+#!/bin/bash
+
+DIRS="$@"
+if [[ -z "$DIRS" ]]; then
+    DIRS="."
+fi
+
+find "$DIRS" \( -name .git -o -name '*.git' \) -type d | \
+    while read repo_dir
+    do
+	if [[ -f "$repo_dir"/config ]]
+	then
+	    # If this is a git-svn repo, use git svn fetch
+	    if grep -q '^\[svn-remote ' "$repo_dir"/config
+	    then
+		echo git svn fetch: $repo_dir
+		GIT_DIR="$repo_dir" git svn fetch
+
+	    # If this is a gc-utils repo, use gc-utils update
+	    elif grep -q '^\[gc-utils\]' "$repo_dir"/config
+	    then
+		echo gc-utils update: $repo_dir
+		(cd "$repo_dir"; gc-utils update)
+
+	    else
+		for remote in $(GIT_DIR="$repo_dir" git remote)
+		do
+		    if [[ $remote != mirror ]]; then
+			echo git fetch: $repo_dir -- $remote
+			GIT_DIR="$repo_dir" git fetch $remote
+		    fi
+		done
+	    fi
+
+	    for remote in $(GIT_DIR="$repo_dir" git remote)
+	    do
+		if [[ $remote == mirror ]]; then
+		    echo git push: $repo_dir -- $remote
+		    GIT_DIR="$repo_dir" git push --mirror $remote
+		fi
+	    done
+	fi
+    done
+    
+gulp
+git add -f .tmp/*
+git add -A
+git commit
+git push
